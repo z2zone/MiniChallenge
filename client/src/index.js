7d7261d399d6754045c37c17d3e0460b4b3f1c8c
hjun1114 <hjun1114@gmail.com>
2018-06-20 11:40:03 -0400

    adding state persistance

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore } from 'redux';
 
 import App from './components/App';
 import reducers from './reducers';
+import throttle from 'lodash/throttle';
+import { loadState, saveState } from './localStorage';
 import './style/style.css';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+function saveToLocalStorage(state) {
+	try {
+		const serializedState = JSON.stringify(state);
+		localStorage.setItem('state', serializedState);
+	} catch (e) {
+		console.log(e);
+	}
+}
+
+function loadFromLocalStorage() {
+	try {
+		const serializedState = localStorage.getItem('state');
+		if(serializedState === null) {
+			return undefined;
+		}
+		return JSON.parse(serializedState);
+	} catch(e) {
+		console.log(e);
+		return undefined;
+	}
+}
+
+const persistedState = loadFromLocalStorage();
+const store = createStore(
+	reducers,
+	persistedState
+);
+
+store.subscribe(()=>{
+	saveToLocalStorage(store.getState());
+});
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
+  <Provider store={store}>
     <App />
   </Provider>
   , document.querySelector('#root'));
