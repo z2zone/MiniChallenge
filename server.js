e2a1c5fb4ceda24ad7dad038f98657a83d61128b
hjun1114 <hjun1114@gmail.com>
2018-06-20 10:21:34 -0400

    express refactoring

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const compression = require('compression');
-const morgan = require('morgan');
+const path = require('path');
 
 const app = express();
 
@@ -12,8 +12,7 @@ app.use(bodyParser.json());
 if(process.env.NODE_ENV === 'production') {
 	app.disable('x-powered-by');
 	app.use(compression());
-	app.use(morgan('common'));
-	app.use(express.static(path.resolve(__dirname, 'client/build')));
+  	app.use(express.static('client/build'));
 	app.get('*', (req, res)=>{
 		res.sendFile(path.resolve(__dirname,'client', 'build', 'index.html'));
 	});
